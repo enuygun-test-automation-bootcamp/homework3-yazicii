113d31e2688ddb4b6a5487cb8a1dc95c56b323c9
Oğuzhan Yazıcı <101544408+yazicii@users.noreply.github.com>
2022-07-24 01:54:24 +0300

    Update UserBrowser.java

--- homework3-yazici3/src/main/java/UserBrowser.java
+++ homework3-yazici3/src/main/java/UserBrowser.java
@@ -6,12 +6,12 @@ public class UserBrowser {
 
     public static void main(String[] args) {
         Utility.setDriverProps(DriverType.CHROME);
-        ChromeOptions chromeOptions = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
 
-        chromeOptions.addArguments("user-data-dirC:\\Users\\Monster\\AppData\\Local\\Google\\Chrome\\User Data");
-        WebDriver chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.get("https://www.enuygun.com/");
-        System.out.println(chromeDriver.getTitle());
+        options.addArguments("user-data-dir=C:/Users/Monster/AppData/Local/Google/Chrome/User Data");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.enuygun.com/");
+        System.out.println(driver.getTitle());
 
     }
 }
